089441fcce6f9c7cf1124b8ef579c0cc500f943c
ledinhvu62 <ledinhvu6@gmail.com>
2023-10-21 01:28:14 +0700

    handle swipe events

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,89 +24,16 @@ const Slider = props => {
     }
 
     const touchAreaRef = useRef()
-    let mouseX, initialX = 0
-    let mouseY, initialY = 0
-    let isSwiped
-    //Events for touch and mouse
-    let events = {
-        mouse: {
-            down: "mousedown",
-            move: "mousemove",
-            up: "mouseup",
-        },
-        touch: {
-            down: "touchstart",
-            move: "touchmove",
-            up: "touchend",
-        },
-    }
-    let deviceType = ""
-    //Detect touch device
-    const isTouchDevice = () => {
-        try {
-            //Try to create TouchEvent (it would fail for desktops and throw error)
-            document.createEvent("TouchEvent")
-            deviceType = "touch"
-            return true
-        } catch (e) {
-            deviceType = "mouse"
-            return false
-        }
-    }
-    //Get left and top of touchArea
-    let rectLeft = touchAreaRef.current ? touchAreaRef.current.getBoundingClientRect().left : 0
-    let rectTop = touchAreaRef.current ? touchAreaRef.current.getBoundingClientRect().top : 0
-    //Get Exact X and Y position of mouse/touch
-    const getXY = (e) => {
-        mouseX = (!isTouchDevice() ? e.pageX : e.touches[0].pageX) - rectLeft
-        mouseY = (!isTouchDevice() ? e.pageY : e.touches[0].pageY) - rectTop
-    };
-    isTouchDevice()
-    //Start Swipe
-    touchAreaRef.current && touchAreaRef.current.addEventListener(events[deviceType].down, (event) => {
-        isSwiped = true
-        //Get X and Y Position
-        getXY(event)
-        initialX = mouseX
-        initialY = mouseY
-    });
-    //Mousemove / touchmove
-    let touchDirection=""
-    touchAreaRef.current && touchAreaRef.current.addEventListener(events[deviceType].move, (event) => {
-        if (!isTouchDevice()) {
-            event.preventDefault()
-        }
-        if (isSwiped) {
-            getXY(event)
-            let diffX = mouseX - initialX
-            let diffY = mouseY - initialY
-            if (Math.abs(diffY) <= Math.abs(diffX)) {
-                touchDirection = diffX > 0 ? "right" : "left"
-            }
-        }
-    });
-    //Stop Drawing
-    touchAreaRef.current && touchAreaRef.current.addEventListener(events[deviceType].up, () => {
-        isSwiped = false
-        if (touchDirection === "right") {
-            prevSlide()
-        }
-        else {
+
+    if (touchAreaRef.current) {
+        touchAreaRef.current.addEventListener('swiped-left', function (e) {
             nextSlide()
-        }
-    })
-    touchAreaRef.current && touchAreaRef.current.addEventListener("mouseleave", () => {
-        isSwiped = false
-        if (touchDirection === "right") {
+        })
+
+        touchAreaRef.current.addEventListener('swiped-right', function (e) {
             prevSlide()
-        }
-        else {
-            nextSlide()
-        }
-    })
-    window.onload = () => {
-        isSwiped = false
-    };
+        })
+    }
 
     useEffect(() => {
         if (props.auto) {
@@ -121,7 +48,7 @@ const Slider = props => {
     }, [nextSlide, timeOut, props])
 
     return (
-        <div ref={touchAreaRef} className="slider touch-area">
+        <div ref={touchAreaRef} className="slider" data-swipe-ignore="false" data-swipe-timeout="500" data-swipe-unit="px" data-swipe-threshold="20">
             {
                 data.map((item, index) => (
                     <div key={index} className={`slider__item ${index === activeSlide ? 'active' : ''}`}>
