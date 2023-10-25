533076c9541dd8bba72dc1926ee66844d12f8740
ledinhvu62 <ledinhvu6@gmail.com>
2023-10-25 15:58:55 +0700

    add href footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,26 @@ import Grid from './Grid'
 import logo from '../assets/images/logo.svg'
 
 const info = [
-    'Mã số thuế: 1234567890',
-    'Địa chỉ: 530 Trương Công Định, Phường 8, Thành phố Vũng Tàu',
-    'Số điện thoại: 0123456789',
-    'Email: cskh@yolo.vn',
+    {
+        label: 'Mã số thuế',
+        value: '1234567890',
+        type: 'text',
+    },
+    {
+        label: 'Địa chỉ',
+        value: '530 Trương Công Định, Phường 8, Thành phố Vũng Tàu',
+        type: 'text',
+    },
+    {
+        label: 'Số điện thoại',
+        value: '0123456789',
+        type: 'phone',
+    },
+    {
+        label: 'Email',
+        value: 'cskh@yolo.vn',
+        type: 'mail',
+    },
 ]
 
 const aboutLinks = [
@@ -79,25 +95,31 @@ const Footer = () => {
                     smCol={1}
                     gap={10}
                 >
-                    <div>
-                        <div className='footer__title'>
+                    <div className='footer__item'>
+                        <div className='footer__item__title'>
                             Công ty Cổ phần Yolo
                         </div>
-                        <div className='footer__content'>
+                        <div className='footer__item__content'>
                             {
                                 info.map((item, index) => (
-                                    <p key={index}>
-                                        {item}
-                                    </p>
+                                    item.type === 'text' ? (
+                                        <p key={index}>
+                                            {`${item.label}: ${item.value}`}
+                                        </p>
+                                    ) : (
+                                        <a key={index} href={`${item.type === 'phone' ? 'tel' : 'mailto'}:${item.value}`}>
+                                            {`${item.label}: ${item.value}`}
+                                        </a>
+                                    )
                                 ))
                             }
                         </div>
                     </div>
-                    <div>
-                        <div className='footer__title'>
+                    <div className='footer__item'>
+                        <div className='footer__item__title'>
                             Về Yolo
                         </div>
-                        <div className='footer__content'>
+                        <div className='footer__item__content'>
                             {
                                 aboutLinks.map((item, index) => (
                                     <p key={index}>
@@ -109,11 +131,11 @@ const Footer = () => {
                             }
                         </div>
                     </div>
-                    <div>
-                        <div className='footer__title'>
+                    <div className='footer__item'>
+                        <div className='footer__item__title'>
                             Hỗ trợ khách hàng
                         </div>
-                        <div className='footer__content'>
+                        <div className='footer__item__content'>
                             {
                                 customerSupportLinks.map((item, index) => (
                                     <p key={index}>
@@ -125,17 +147,17 @@ const Footer = () => {
                             }
                         </div>
                     </div>
-                    <div className='footer__about'>
+                    <div className='footer__item'>
                         <Link to='/'>
-                            <img className='footer__logo' src={logo} alt='Logo Yolo' />
+                            <img className='footer__item__logo' src={logo} alt='Logo Yolo' />
                         </Link>
-                        <div className='footer__title'>
+                        <div className='footer__item__title'>
                             Theo dõi chúng tôi tại
                         </div>
-                        <div className='footer__content'>
+                        <div className='footer__item__content'>
                             {
                                 socialNetworkLinks.map((item, index) => (
-                                    <a key={index} className='footer__social-link' href={item.link} target='_blank' rel='noopener noreferrer'>
+                                    <a key={index} className='footer__item__social-link' href={item.link} target='_blank' rel='noopener noreferrer'>
                                         <i className={item.icon}></i>
                                     </a>
                                 ))
