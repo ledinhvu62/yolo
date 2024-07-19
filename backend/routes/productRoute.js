6cd60fb9765194c7775ceb3c067b9be41a6de860
ledinhvu62 <ledinhvu6@gmail.com>
2024-07-19 20:13:44 +0700

    fix bug 20131907

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,17 +2,22 @@ import express from 'express'
 import { addProduct, listProduct, removeProduct, getProduct, getRelatedProducts } from '../controllers/productController.js'
 import multer from 'multer'
 
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import cloudinary from '../config/cloudinary.js'
+
 const productRouter = express.Router()
 
 // Image Storage Engine
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}${file.originalname}`)
-    }
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'products',
+        format: async (req, file) => 'jpg', // supports promises as well
+        public_id: (req, file) => `${Date.now()}_${file.originalname}`,
+    },
 })
 
-const upload = multer({storage:storage})
+const upload = multer({ storage })
 
 productRouter.post('/add', upload.array('image'), addProduct)
 productRouter.get('/list', listProduct)
