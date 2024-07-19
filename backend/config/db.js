d38f53ce5509d233ac86e704f3f89a7839613362
ledinhvu62 <ledinhvu6@gmail.com>
2024-07-19 21:06:40 +0700

    fix bug 21061907

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI).then(() => console.log('DB connected'))
