bc57eb9f4d7fc29c40525e382df9f64e203559ea
ledinhvu62 <ledinhvu6@gmail.com>
2024-07-24 20:03:14 +0700

    fix bug 20032407

--- server/index.js
+++ server/index.js
@@ -3,53 +3,29 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import 'dotenv/config.js'
 
-import routes from "./src/routes/index.js"
-import mongoose from "mongoose"
+import routes from './src/routes/index.js'
+import mongoose from 'mongoose'
 import http from 'http'
 
-/* // App config
 const app = express()
-const port = 4000
 
-// Middleware
 app.use(cors())
-app.use(express.json())
-
-// DB Connection
-connectDB()
-
-// API Endpoints
-app.use('/api/product', productRouter)
-app.use('/api/user', userRouter)
-app.use('/api/cart', cartRouter)
-app.use('/images', express.static('uploads'))
-
-app.get('/', (req, res) => {
-  res.send('API Working')
-})
-
-app.listen(port, () => {
-    console.log('Server started')
-}) */
-
-const app = express()
-const corsOptions = {
-    origin: '*', // Your frontend domain
-}
-
-app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use("/api/v1", routes)
+app.use('/api/v1', routes)
+
+app.get('/', (req, res) => {
+    res.send('API Working')
+})
 
 const port = process.env.PORT || 4000
 
 const server = http.createServer(app)
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log("Mongodb connected");
+    console.log('Mongodb connected');
     server.listen(port, () => {
         console.log(`Server is listening on port ${port}`)
     })
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -14,7 +14,6 @@ const productRouter = express.Router()
     }
 }) */
 
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
