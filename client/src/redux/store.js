ef1fde74af0934ef5728ab8dcf7676b523c63c82
ledinhvu62 <ledinhvu6@gmail.com>
2024-07-25 00:01:02 +0700

    rename file

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,16 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import cartItemsSlice from './features/cartItemsSlice'
 import keywordSearchSlice from './features/keywordSearchSlice'
-import tokenSlice from './features/tokenSlice'
 import productListSlice from './features/productListSlice'
+import tokenSlice from './features/tokenSlice'
 import urlSlice from './features/urlSlice'
 
 export const store = configureStore({
     reducer: {
         cartItems: cartItemsSlice,
         keywordSearch: keywordSearchSlice,
-        token: tokenSlice,
         productList: productListSlice,
+        token: tokenSlice,
         url: urlSlice,
     }
 })
